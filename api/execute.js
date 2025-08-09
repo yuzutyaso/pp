f6202147aa1ce74fdf32e392c7946a98177cf52a
yuzutyaso <f.kokori85@gmail.com>
2025-08-09 12:07:04 +0900

    Update execute.js

--- api/execute.js
+++ api/execute.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 
 const allowedCommands = [
     'ls', 'echo', 'whoami', 'pwd', 'date',
-    'uname', 'df', 'free', 'expr',　'bc'
+    'uname', 'df', 'free', 'expr',　'bc', 'shuf'
 ];
 
 module.exports = (req, res) => {
